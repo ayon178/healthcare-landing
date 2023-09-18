969125ea1211fd6dc882ab856364fdb8813e76df
AyonJD <ayonjodder177@gmail.com>
2023-09-18 12:59:34 +0600

    Initialized with nav

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,68 @@
+"use client"
+
+import React, { useEffect, useRef } from 'react'
+import {BiMenuAltRight} from 'react-icons/bi'
+import MobileMenu from './MobileMenu'
+import gsap from 'gsap'
+import Link from 'next/link'
+
+const Navbar = () => {
+
+    const refMobile = useRef(null);
+
+    useEffect(() => {
+        gsap.from(refMobile.current, {
+            xPercent: -100
+        })
+    }, [])
+
+    const openMenu = () => {
+        gsap.to(refMobile.current, {
+            xPercent: 0,
+            duration: 1.2,
+            ease: 'power2.out'
+        })
+    }
+
+    const closeMenu = () => {
+        gsap.to(refMobile.current, {
+            xPercent: -100,
+            duration: 1.2,
+            ease: 'power2.out'
+        })
+    }
+
+    return (
+      <div className='bg-primary py-3'>
+    <div className='container mx-auto flex flex-row justify-between'>
+          <Link href='/#' className='font-semibold text-2xl text-primaryText'>LOGO</Link>
+        <ul className='flex-row items-center gap-4 text-primaryText hidden md:flex'>
+            <li>
+                  <Link href='/#'> Menu 1</Link>
+            </li>
+            <li>
+                  <Link href='/#'> Menu 2</Link>
+            </li>
+            <li>
+                  <Link href='/#'> Menu 3</Link>
+            </li>
+            <li>
+                <Link href='/#'> Menu 4</Link>
+              </li>
+              <li>
+                  <button className='bg-white text-slate-600 px-3 py-2 rounded-md'>Book Appointment</button>
+              </li>
+        </ul>
+        <BiMenuAltRight className='text-primaryText text-4xl md:hidden' onClick={openMenu}/>
+
+        <MobileMenu 
+            ref = {refMobile}
+            closeMenu = {closeMenu}
+        />
+
+            </div>
+            </div>
+  )
+}
+
+export default Navbar
