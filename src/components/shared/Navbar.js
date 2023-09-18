976d3a9a2c0482a49514b5df090e2972f71d4c2b
AyonJD <ayonjodder177@gmail.com>
2023-09-18 13:39:49 +0600

    nav done

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,67 +1,111 @@
-"use client"
+'use client'
 
 import React, { useEffect, useRef } from 'react'
-import {BiMenuAltRight} from 'react-icons/bi'
+import { BiMenuAltRight } from 'react-icons/bi'
 import MobileMenu from './MobileMenu'
 import gsap from 'gsap'
 import Link from 'next/link'
 
 const Navbar = () => {
+  const refMobile = useRef(null)
 
-    const refMobile = useRef(null);
+  useEffect(() => {
+    gsap.from(refMobile.current, {
+      xPercent: -100,
+    })
+  }, [])
 
-    useEffect(() => {
-        gsap.from(refMobile.current, {
-            xPercent: -100
-        })
-    }, [])
+  const openMenu = () => {
+    gsap.to(refMobile.current, {
+      xPercent: 0,
+      duration: 1.2,
+      ease: 'power2.out',
+    })
+  }
 
-    const openMenu = () => {
-        gsap.to(refMobile.current, {
-            xPercent: 0,
-            duration: 1.2,
-            ease: 'power2.out'
-        })
-    }
+  const closeMenu = () => {
+    gsap.to(refMobile.current, {
+      xPercent: -100,
+      duration: 1.2,
+      ease: 'power2.out',
+    })
+  }
 
-    const closeMenu = () => {
-        gsap.to(refMobile.current, {
-            xPercent: -100,
-            duration: 1.2,
-            ease: 'power2.out'
-        })
-    }
+  const handleMenuItemHover = event => {
+    // Create a unique hover animation using GSAP
+    gsap.to(event.target, {
+      opacity: 0.8, // Reduce opacity on hover
+      duration: 0.3,
+      ease: 'power2.out',
+    })
+  }
 
-    return (
-      <div className='bg-primary py-3'>
-    <div className='container mx-auto flex flex-row justify-between'>
-          <Link href='/#' className='font-semibold text-2xl text-primaryText'>LOGO</Link>
-        <ul className='flex-row items-center gap-4 text-primaryText hidden md:flex'>
-            <li>
-                  <Link href='/#'> Menu 1</Link>
-            </li>
-            <li>
-                  <Link href='/#'> Menu 2</Link>
-            </li>
-            <li>
-                  <Link href='/#'> Menu 3</Link>
-            </li>
-            <li>
-                <Link href='/#'> Menu 4</Link>
-              </li>
-              <li>
-                  <button className='bg-white text-slate-600 px-3 py-2 rounded-md'>Book Appointment</button>
-              </li>
+  const handleMenuItemLeave = event => {
+    // Restore the menu item on mouse leave
+    gsap.to(event.target, {
+      opacity: 1, // Restore original opacity
+      duration: 0.3,
+      ease: 'power2.out',
+    })
+  }
+
+  return (
+    <div className="bg-primary py-3">
+      <div className="container mx-auto flex flex-row justify-between">
+        <Link href="/#" className="font-semibold text-2xl text-primaryText">
+          LOGO
+        </Link>
+        <ul className="flex-row items-center gap-4 text-primaryText hidden md:flex">
+          <li>
+            <Link
+              href="/#"
+              onMouseEnter={handleMenuItemHover}
+              onMouseLeave={handleMenuItemLeave}
+            >
+              Menu 1
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/#"
+              onMouseEnter={handleMenuItemHover}
+              onMouseLeave={handleMenuItemLeave}
+            >
+              Menu 2
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/#"
+              onMouseEnter={handleMenuItemHover}
+              onMouseLeave={handleMenuItemLeave}
+            >
+              Menu 3
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/#"
+              onMouseEnter={handleMenuItemHover}
+              onMouseLeave={handleMenuItemLeave}
+            >
+              Menu 4
+            </Link>
+          </li>
+          <li>
+            <button className="bg-white text-slate-600 px-3 py-2 rounded-md">
+              Book Appointment
+            </button>
+          </li>
         </ul>
-        <BiMenuAltRight className='text-primaryText text-4xl md:hidden' onClick={openMenu}/>
-
-        <MobileMenu 
-            ref = {refMobile}
-            closeMenu = {closeMenu}
+        <BiMenuAltRight
+          className="text-primaryText text-4xl md:hidden"
+          onClick={openMenu}
         />
 
-            </div>
-            </div>
+        <MobileMenu ref={refMobile} closeMenu={closeMenu} />
+      </div>
+    </div>
   )
 }
 
